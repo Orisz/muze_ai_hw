647fedce55ed9fdb9ae38e63629d5f237945bfe2
Ori Sztyglic <orisz@mobileye.com>
2023-05-04 21:24:02 +0300

    done with algo

--- find_defects.py
+++ find_defects.py
@@ -1,28 +1,81 @@
 import argparse
 from utils.im_utils import *
-from utils.hist_thresh import GHT
+import scipy.stats as stats
+import os
 
-def find_defects(ref_im, query_img_aligned):
-    diff_im = ref_im - query_img_aligned
-    diff_im = cv2.normalize(diff_im  , None, 0, 255, cv2.NORM_MINMAX)
-    ret, otsu = cv2.threshold(diff_im, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+def save_mask(binary_mask:np.ndarray, dst:str, im_path:str, conf_lvl:int):
+    """
+    saves the musk given some paras such as destination image name etc.
+    """
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    im_name = im_path.split("/")[-1]
+    im_full_loc = os.path.join(dst, f"mask_{conf_lvl}%_conf_lvl_"+im_name)
+    cv2.imwrite(im_full_loc, binary_mask)
+
+
+def find_defects(ref_im, query_im, M, confidence_level):
+    """
+    Given the images, the transformation, and the desired confidence level
+    align the images
+    =>
+    Find the defects with:
+    p(pixel_defect | x,y) >= confidence_level
+    =>
+    Return a binary musk of the defects
+    """
+    assert type(confidence_level)==int and confidence_level>=0 and confidence_level<=100,\
+          "'confidence_level' must be an integer in range [0,100]"
+    
+    # Apply the transformation matrix to the target image
+    img_aligned = cv2.warpAffine(ref_im, M, (ref_im.shape[1], ref_im.shape[0]))
+
+    # Estimate the noise level of the image
+    query_sigma = estimate_sigma(query_im)
+
+    # Pad the blank area after translation to avoid false detection
+    x_shift     = int(M[0, 2])
+    y_shift     = int(M[1, 2])
+    if x_shift <= 0:
+        img_aligned[:, x_shift:] = query_im[:, x_shift:]
+    else:
+        img_aligned[:, :x_shift] = query_im[:, :x_shift]
+    if y_shift <= 0:
+        img_aligned[y_shift:, :] = query_im[y_shift:, :]
+    else:
+        img_aligned[:y_shift, :] = query_im[:y_shift, :]
+
+    # Find the diff image
+    diff = cv2.absdiff(query_im, img_aligned)
+
+    # Find the threshold w.r.t. the desired confidence level 
+    thresh = stats.norm.ppf((100 + confidence_level) / 200, scale=query_sigma)
+
+    # Threshold the image to get the binary mask
+    _, binary_mask = cv2.threshold(diff, thresh, 255, cv2.THRESH_BINARY)
+
+    return binary_mask
+
 def run_pipe(args):
     # Read the images
     ref_im, query_im = read_images(args.ref_image_path, args.query_image_path)
 
-    # Estimate the noise level of the image
-    # ref_sigma   = estimate_sigma(ref_im)
-    # query_sigma = estimate_sigma(query_im)
-
     # Find transformation between the query image to the ref image
-    ref_im, query_img_aligned = align_images(ref_im, query_im, args.is_debug)
-    find_defects(ref_im, query_img_aligned)
+    M = find_transformation(ref_im, query_im, args.is_debug)
+
+    # Find the defects
+    binary_mask = find_defects(ref_im, query_im, M, args.confidence_level)
+
+    save_mask(binary_mask, args.dst_path, args.query_image_path, args.confidence_level)
 
 def main():
     # Create an ArgumentParser object to get the image paths as input arguments
     parser = argparse.ArgumentParser(description='Defect Finder')
     parser.add_argument('--ref_image_path', type=str, help='Path to the reference image')
     parser.add_argument('--query_image_path', type=str, help='Path to the query image')
+    parser.add_argument('--confidence_level', type=int, help='desired conf level, provide an integer within the range [0,100]')
+    parser.add_argument('--dst_path', type=str, help='location folder to save the results in')
     parser.add_argument('--is_debug', type=bool, default=False, help='Debug falg, set true for debug properties')
     args = parser.parse_args()
 
--- utils/im_utils.py
+++ utils/im_utils.py
@@ -17,7 +17,10 @@ def read_images(ref_im_path:str, query_im_path:str, is_debug:bool=False) -> Tupl
         cv2.waitKey()
         cv2.imshow('Query Image', query_image)
         cv2.waitKey()
-
+    
+    # Convert to grayscale
+    ref_image     = cv2.cvtColor(ref_image   , cv2.COLOR_BGR2GRAY)
+    query_image   = cv2.cvtColor(query_image , cv2.COLOR_BGR2GRAY)
     return ref_image, query_image
 
 
@@ -43,121 +46,45 @@ def estimate_sigma(image:np.ndarray) -> float:
 
     return sigma
 
-def align_images(ref_im:np.ndarray,
+def find_transformation(ref_im:np.ndarray,
                               query_im:np.ndarray,
-                              is_debug:bool=False)\
-    -> Tuple[np.ndarray, np.ndarray]:
+                              is_debug:bool=False) -> np.ndarray:
     """
     Finds a rigid transformation between the query image to the ref image
-    and return the ref image and the transformed query image.
+    and return the transformation.
     """
-    # Convert the images to grayscale
-    ref_img     = cv2.cvtColor(ref_im   , cv2.COLOR_BGR2GRAY)
-    query_img   = cv2.cvtColor(query_im , cv2.COLOR_BGR2GRAY)
 
-    # normalize images:
-    # ref_img     = cv2.normalize(ref_img  , None, 0, 255, cv2.NORM_MINMAX)
-    # query_img   = cv2.normalize(query_img, None, 0, 255, cv2.NORM_MINMAX)
-    # query_img = ref_img
+    ref_img   = apply_LoG(ref_im)
+    query_img = apply_LoG(query_im)
 
-    # ref_img   = apply_LoG(ref_img)
-    # query_img = apply_LoG(query_img)
+    # Define the template size
+    y, x = ref_img.shape
+    min_size = min(x, y)
+    size = min_size // 2
+    template_size = (size, size)
 
-    use_sift = False
-    if use_sift:
-        # Create a SIFT feature detector object
-        sift = cv2.xfeatures2d.SIFT_create()
+    # Define the template from the reference image
+    top_left = (ref_img.shape[0] // 2 - template_size[0] // 2, ref_img.shape[1] // 2 - template_size[1] // 2)
+    template = query_img[top_left[0]:top_left[0]+template_size[0], top_left[1]:top_left[1]+template_size[1]]
 
-        # Detect and compute keypoints and descriptors for both images
-        kp1, des1 = sift.detectAndCompute(ref_img, None)
-        kp2, des2 = sift.detectAndCompute(query_img, None)
+    # Perform template matching to determine the shift between the two images
+    result = cv2.matchTemplate(ref_img, template, cv2.TM_CCOEFF)
+    _, _, _, max_loc = cv2.minMaxLoc(result)
 
-        # Create a brute force matcher object
-        bf = cv2.BFMatcher()
+    # Get the x and y shift from the template matching result
+    shift_x = top_left[1] - max_loc[0]
+    shift_y = top_left[0] - max_loc[1]
 
-        # Match the descriptors between the two images
-        matches = bf.knnMatch(des1, des2, k=2)
+    # Create a transformation matrix to shift the target image
+    M = np.float32([[1, 0, shift_x], [0, 1, shift_y]])
 
-        # Apply the ratio test to filter out ambiguous matches
-        good_matches = []
-        for m, n in matches:
-            if m.distance < 0.75 * n.distance:
-                good_matches.append(m)
+    return M
 
-        # Extract the corresponding keypoint coordinates for the good matches
-        pts1 = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-        pts2 = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-
-        # Estimate the homography matrix between the two images using RANSAC algorithm
-        H, _ = cv2.findHomography(pts2, pts1, cv2.RANSAC, 5.0)
-
-        # Warp the second image to align it with the first image using the estimated homography matrix
-        warped_img = cv2.warpPerspective(query_img, H, (ref_img.shape[1], ref_img.shape[0]))
-
-        # Display the results
-        cv2.imshow("Image 1", ref_img)
-        cv2.imshow("Image 2", query_img)
-        cv2.imshow("Warped Image 2", warped_img)
-        res = ref_img - warped_img
-        min_res = res.min()
-        res = res + min_res
-        cv2.imshow("diff", res)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    else:
-
-        # Define the template size
-        template_size = (100, 100)
-
-        # Define the template from the reference image
-        top_left = (query_img.shape[0] // 2 - template_size[0] // 2, query_img.shape[1] // 2 - template_size[1] // 2)
-        template = ref_img[top_left[0]:top_left[0]+template_size[0], top_left[1]:top_left[1]+template_size[1]]
-
-        # Perform template matching to determine the shift between the two images
-        result = cv2.matchTemplate(query_img, template, cv2.TM_CCOEFF)
-        _, _, _, max_loc = cv2.minMaxLoc(result)
-
-        # Get the x and y shift from the template matching result
-        shift_x = top_left[1] - max_loc[0]
-        shift_y = top_left[0] - max_loc[1]
-
-        # Create a transformation matrix to shift the target image
-        M = np.float32([[1, 0, shift_x], [0, 1, shift_y]])
-
-        # Apply the transformation matrix to the target image
-        img_aligned = cv2.warpAffine(query_img, M, (query_img.shape[1], query_img.shape[0]))
-
-        # # Display the aligned image
-        # cv2.imshow('Aligned Image', img_aligned)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-         # Display the results
-        # cv2.imshow("Image 1", ref_img)
-        # cv2.imshow("Image 2", query_img)
-        # cv2.imshow("Warped Image 2", img_aligned)
-        # res = ref_img - img_aligned
-        # min_res = res.min()
-        # res = res + min_res
-        # cv2.imshow("diff", res)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
-    return ref_img, img_aligned
-
-# def apply_LoG(image:np.ndarray) -> np.ndarray:
-#     # Apply Gaussian blur to the image
-#     # blurred = cv2.GaussianBlur(image, (9, 9), 0)
-#     blurred = cv2.medianBlur(image, 3)
-
-#     # # Apply Laplacian of Gaussian (LoG) filter to the blurred image
-#     # log = cv2.Laplacian(blurred, cv2.CV_64F)
-
-#     # # Convert the result to uint8 and normalize the values to the range [0, 255]
-#     # log = cv2.convertScaleAbs(log)
-#     # cv2.normalize(log, log, 0, 255, cv2.NORM_MINMAX)
-
-#     # Display the result
-#     # cv2.imshow("LoG Filtered Image", blurred)
-#     # cv2.waitKey(0)
-#     # cv2.destroyAllWindows()
-#     return blurred
+def apply_LoG(image:np.ndarray) -> np.ndarray:
+    # Apply Gaussian blur to the image
+    im = cv2.GaussianBlur(image, (3, 3), 0)
+    # Apply Laplacian to sharpen it
+    im = cv2.Laplacian(im, cv2.CV_16S, ksize=3)
+    # Back to uint8
+    im = cv2.convertScaleAbs(im)
+    return im
